3dee8f9901f0f60545b648dc636f24eb344207f0
AcaSolidity23 <alazrevic381@gmail.com>
2024-11-01 16:49:46 +0100

    final

---
+++ tkinter_input.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+ourWindow = Tk()
+ourTextField1 = Entry(ourWindow)
+ourTextField1.pack()
+
+def ourFunc():
+    textEntered = ourTextField1.get()    
+    ourLab = Label(ourWindow, text = textEntered,fg='aqua',bg='black').pack()
+    
+ourBttn = Button(ourWindow, text= 'Enter')
+command = ourFunc.pack()
+
+ourWindow.mainloop()
+    
