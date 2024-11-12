8f791df5bad90fe1ae483b6869a141a2ba7e6c56
AcaSolidity23 <alazrevic381@gmail.com>
2024-11-12 13:50:59 +0100

    corr_bs

--- source/weather.py
+++ source/weather.py
@@ -2,5 +2,5 @@ import daily, weekly
 
 print('Daily forecast: ', daily.forecast())
 print('Weekly forecast: ')
-for number, outlook in enumerate(weekly.forecast(),1):
+for number, outlook in enumerate(weekly.forecast(), 1):
     print(number, outlook)
