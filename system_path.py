b5eb0279493b362b7fd353d94e2b240c1265dbf2
AcaSolidity23 <alazrevic381@gmail.com>
2024-11-13 16:29:27 +0100

    corr

--- system_path.py
+++ system_path.py
@@ -5,5 +5,5 @@ for k in sys.path:
 def add_to_path(folder_path):
     sys.path.append(folder_path)
 
-fp = "C:\\Users\\alazarevic\\AppData\\Local\\Programs\\Python\\Python312\\Scripts"
+fp = 'C:/Users/alazarevic/AppData/Local/Programs/Python/Python312/Scripts'
 add_to_path(fp)
