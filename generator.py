c14992c6600ad011096017c8ef66db7890971ec0
AcaSolidity23 <alazrevic381@gmail.com>
2024-11-13 16:20:57 +0100

    add_to_path

---
+++ generator.py
@@ -0,0 +1,4 @@
+def rg_summ(opseg):
+    return sum(range(1, opseg))
+
+print(rg_summ(10))
--- system_path.py
+++ system_path.py
@@ -1,2 +1,9 @@
 import sys
-print (sys.path)
+for k in sys.path:
+    print(k)
+
+def add_to_path(folder_path):
+    sys.path.append(folder_path)
+
+fp = "C:\\Users\\alazarevic\\AppData\\Local\\Programs\\Python\\Python312\\Scripts"
+add_to_path(fp)
