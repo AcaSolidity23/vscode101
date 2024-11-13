325e6bde6789a8a07255af2657059a77e2497cb7
AcaSolidity23 <alazrevic381@gmail.com>
2024-11-13 14:32:22 +0100

    corr

--- decorator.py
+++ decorator.py
@@ -1,17 +1,17 @@
-def document_it(func):
-    def new_function(*args,**kwargs):
-        print('Running function: ',func.__name__)
-        print('Positional arguments:', args)
-        print('Keyword arguments:', kwargs)
-        result = func(args, kwargs)
-        print*('Result:', result)
-        return result
-    return new_function
-
 def add_ints(a, b):
     return a + b
 
-add_ints(3, 5)
+print('Simple func:', add_ints(3, 5))
+
+def document_it(func):
+    def new_function(*args,**kwargs):
+        print('Running function: ', func.__name__)
+        print('Positional arguments:', args)
+        print('Keyword arguments:', kwargs)
+        result = func(*args, **kwargs)
+        print('Result:', result)
+        return result
+    return new_function
 
 cooler_add_ints = document_it(add_ints)
 cooler_add_ints(3, 5)
