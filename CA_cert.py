02bcf53c2f5d25db2b25dea572e5f2bdbf1ac4be
AcaSolidity23 <alazrevic381@gmail.com>
2024-12-10 13:29:21 +0100

    other_path

--- CA_cert.py
+++ CA_cert.py
@@ -3,7 +3,11 @@ import sys
 import certifi
 import requests as r
 print(r.certs.where())
+print (str(certifi.where()))
+
 
 os.environ['SSL_CERT_FILE'] = r.certs.where()
+#os.environ['SSL_CERT_FILE'] = str(certifi.where())
+
 os.environ['REQUESTS_CA_BUNDLE'] =
 os.path.join(os.path.dirname(sys.argv[0]), certifi.where())
