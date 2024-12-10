2c6d206828ee1c96646daf40fb698a4e8169cdc1
AcaSolidity23 <alazrevic381@gmail.com>
2024-12-10 14:07:54 +0100

    dict

---
+++ lst_comprehension.py
@@ -0,0 +1,8 @@
+a=[1,2,3,4,5,6,7,8,9,10]
+squares = [x**2 for x in a if x % 2 == 0]
+print (squares)
+
+even_squares_dict = {x: x**2 for x in a if x % 2 == 0}
+threes_cubed_set = {x: x**3 for x in a if x % 3 == 0}
+print(even_squares_dict)
+print(threes_cubed_set)
